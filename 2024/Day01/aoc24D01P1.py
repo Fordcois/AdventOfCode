fa90e789a9d107223d55a016a668f96ae7cf9dc3
Sam Ford <samueljamesford@googlemail.com>
2024-12-01 14:32:20 +0000

    Completes 2024 Day1 part1

---
+++ 2024/Day01/aoc24D01P1.py
@@ -0,0 +1,32 @@
+# Adjust Import Paths
+import sys
+import os
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, project_root)
+# Import Utilities
+from utils.ParseTXTAsList import Parse_Txt_as_List
+
+# Substitue test/real to switch inputs
+data_set = 'real'
+data = Parse_Txt_as_List(f'{data_set}_input.txt')
+
+def solve():
+    total_difference = 0
+    list_a = []
+    list_b = []
+    for line in data:
+        line_numbers = [int(x) for x in line.split('   ')]
+        list_a.append(line_numbers[0])
+        list_b.append(line_numbers[1])
+
+    list_a.sort()
+    list_b.sort()
+
+    for i in range (len(list_a)):
+        total_difference += abs(list_a[i]-list_b[i])
+    
+    print (f'Total Difference is {total_difference}')
+
+solve()
+
+
