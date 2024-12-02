19b606e1dbf198ccea47f682341287c5c5b34bde
Sam Ford <samueljamesford@googlemail.com>
2024-12-02 16:38:00 +0000

    Removes unused tests

--- 2024/Day01/test_aoc2401Part01.py
+++
@@ -1,5 +0,0 @@
-from .aoc2401Part01 import *
-# Test Suites are included to aid during development and debugging and may not offer a complete overview and solution
-
-def test_connection():
-    assert 'Hello World' == 'Hello World'
