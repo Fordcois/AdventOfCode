55da3640c7922853f321eb424a530ed57601076b
Sam Ford <samueljamesford@googlemail.com>
2025-12-10 00:33:45 +0000

    Solves Day 9 Part 1 and Two

--- 2025/Day09/aoc202509Part01.py
+++ 2025/Day09/aoc202509Part01.py
@@ -11,7 +11,6 @@ from utilities.read_file_as_str_list import file_as_list_of_strings
 # Substitue test/real to switch inputs
 data_set = 'real'
 data = file_as_list_of_strings(f'{data_set}_input.txt')
-# Swap X/Y Coordinates from input
 data = [[int(a), int(b)] for a, b in (line.split(',') for line in data)]
 
 def find_area(corners):
@@ -24,9 +23,8 @@ def find_area(corners):
 def solve():
 
     combos = list(itertools.combinations(data, 2))
-# Sort by disnttance
-    data_sorted = [f'{find_area(x)} - {x}' for x in sorted(combos, key=lambda x: find_area(x),reverse=True)]
-    print(data_sorted[1])
+    data_sorted = [find_area(x) for x in sorted(combos, key=lambda x: find_area(x),reverse=True)]
+    return data_sorted[0]
 
 
-solve()
+print (f'The answer is:{solve()}')
--- 2025/Day09/aoc202509Part02.py
+++ 2025/Day09/aoc202509Part02.py
@@ -1,16 +1,48 @@
 import sys
 import os
+import itertools
 # Adjust Import Paths
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 # Import Utilities
 from utilities.read_file_as_str_list import file_as_list_of_strings
 
 # Substitue test/real to switch inputs
-data_set = 'test'
+data_set = 'real'
 data = file_as_list_of_strings(f'{data_set}_input.txt')
+red_tiles = [(int(a), int(b)) for a, b in (line.split(',') for line in data)]
+
+
+def find_area(corners):
+    y_distance = abs(corners[0][0]-corners[1][0])+1
+    x_distance = abs(corners[0][1]-corners[1][1])+1
+    return x_distance * y_distance
+
+
+def is_valid(pair, edge):
+    (bx1, by1), (bx2, by2) = pair
+    rx_min, rx_max = min(bx1, bx2), max(bx1, bx2)
+    ry_min, ry_max = min(by1, by2), max(by1, by2)
+
+    for edge in edge:
+        (lx1, ly1), (lx2, ly2) = edge
+        lx_min, lx_max = min(lx1, lx2), max(lx1, lx2)
+        ly_min, ly_max = min(ly1, ly2), max(ly1, ly2)
+
+        if not (lx_max <= rx_min or lx_min >= rx_max or ly_max <= ry_min or ly_min >= ry_max):
+            return False  
+    return True  
+
 
 # Puzzle
 def solve():
-    return
+    border = [ (red_tiles[i], red_tiles[(i+1)%len(red_tiles)]) for i in range(len(red_tiles)) ]
 
-solve()
+    pairs = list(itertools.combinations(red_tiles, 2))
+    data_sorted = sorted(pairs, key=lambda x: find_area(x), reverse=True)
+
+    for pair in data_sorted:
+        if is_valid(pair, border):
+            area = find_area(pair)
+            return area
+
+print (f'The answer is {solve()}')
